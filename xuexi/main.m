681f59126641dd0b722eef7e391117ccae7672f9
NarutoMac <narutomac@narutomacdemacbook-air.local>
2017-11-14 15:59:39 +0000

    Initial Commit

---
+++ xuexi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xuexi
+//
+//  Created by NarutoMac on 2017/11/14.
+//  Copyright © 2017年 NarutoMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
